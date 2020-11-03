4ee6b07cf0c6cf3bb46a2044880e4d0da29c4ab1
Soaib Khan <soaibkhan38@gmail.com>
2020-11-03 11:01:57 +0530

    Bug Fixes

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
+import os
 import requests
 import feedparser
 from datetime import timedelta, datetime
 from dateutil import parser
 from pprint import pprint
 from time import sleep
-BOT_TOKEN='1349889773:AAGDiGrrt3hEZVj7672kHSXbmDfp5Z8bKkE'
+BOT_TOKEN=os.environ.get("BOT_TOKEN")
 CHANNEL_ID='@AnimeReleaseAlert'
 FEED_URL="http://animeschedule.net/subrss.xml"
 #FEED_URL='https://honeysanime.com/feed/'
-i=1
+#i=1
 def send_msg(title,msg):
     print("Detected a new release!")
     headers={'Connection':'close'}
-    requests.Session().post(f'https://api.telegram.org/bot'+BOT_TOKEN+'/sendMessage?chat_id='+CHANNEL_ID+'&text='+title+'%0A'+msg, headers=headers, verify=False)
+    requests.Session().post(f'https://api.telegram.org/bot'+BOT_TOKEN+'/sendMessage?chat_id='+CHANNEL_ID+'&text='+title+'%0A'+msg, headers=headers)
     
 def main():
-    print("running..."+str(i), end='\r')
+    #print("running..."+str(i), end='\r')
     news=feedparser.parse(FEED_URL)
     for entry in news.entries:
         parsed_date = parser.parse(entry.published).replace(tzinfo=None)
@@ -29,8 +30,8 @@ def main():
             print(entry.links[0].href)    
             sleep(60)
 if __name__=="__main__":
-    i=1
+    #i=1
     while(True):
         main()
         sleep(2*60)
-        i+=1
+        #i+=1
