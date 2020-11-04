b59d46ab134b76ee2e56e717a83657fcd81588d2
Soaib Khan <soaibkhan38@gmail.com>
2020-11-04 18:41:11 +0530

    Fixed Duplicate Post Bug #1

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from time import sleep
 BOT_TOKEN=os.environ.get("BOT_TOKEN")
 CHANNEL_ID='@AnimeReleaseAlert'
 FEED_URL="http://animeschedule.net/subrss.xml"
+prev=None
 #FEED_URL='https://honeysanime.com/feed/'
 #i=1
 def send_msg(title,msg):
@@ -23,15 +24,17 @@ def main():
         #parsed_date = (parsed_date - timedelta(hours=8)).replace(tzinfo=None)
         now_date = datetime.utcnow()
 
-        published_3_minutes_ago = now_date - parsed_date < timedelta(minutes=3)
+        published_3_minutes_ago = now_date - parsed_date < timedelta(minutes=2)
         #print(published_5_minutes_ago)
-        if published_3_minutes_ago:
+        if published_3_minutes_ago AND entry.title!=prev:
+            if prev==None: prev=entry.title
             send_msg(entry.title,entry.links[0].href)
             print(entry.links[0].href)    
-            sleep(60)
+            #sleep(60)
 if __name__=="__main__":
     #i=1
     while(True):
+        prev=None
         main()
         sleep(2*60)
         #i+=1
